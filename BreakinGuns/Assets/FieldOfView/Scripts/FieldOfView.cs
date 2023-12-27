9517c9f479abecd289a8ba6d4a024a6803936e01
RedLobo1 <132663385+RedLobo1@users.noreply.github.com>
2023-12-27 22:43:15 +0100

    Fixed field of view - can be seen only in debug + character movement
    Character movement created

--- BreakinGuns/Assets/FieldOfView/Scripts/FieldOfView.cs
+++ BreakinGuns/Assets/FieldOfView/Scripts/FieldOfView.cs
@@ -1,71 +1,67 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-
-    Thank you for downloading this package
-    I hope you find it useful in your projects
-    If you have any questions let me know
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
-
-using System.Collections;
-using System.Collections.Generic;
+﻿using CodeMonkey.Utils;
 using UnityEngine;
-using CodeMonkey.Utils;
 
-public class FieldOfView : MonoBehaviour {
-
+public class FieldOfView : MonoBehaviour
+{
     [SerializeField] private LayerMask layerMask;
-    private Mesh mesh;
+    [SerializeField] private NPC_MovementScript _movementScript;
     private float fov;
-    private float viewDistance;
+
+    private Mesh mesh;
     private Vector3 origin;
     private float startingAngle;
+    private float viewDistance;
 
-    private void Start() {
+    private void Start()
+    {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        fov = 360f;
+        fov = 360;
         viewDistance = 5f;
         origin = Vector3.zero;
     }
 
-    private void LateUpdate() {
-        int rayCount = 100;
-        float angle = startingAngle;
-        float angleIncrease = fov / rayCount;
+    private void LateUpdate()
+    {
+        var rayCount = 50;
+        var angle = startingAngle;
+        var angleIncrease = fov / rayCount;
 
-        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
-        Vector2[] uv = new Vector2[vertices.Length];
-        int[] triangles = new int[rayCount * 3];
+        var vertices = new Vector3[rayCount + 1 + 1];
+        var uv = new Vector2[vertices.Length];
+        var triangles = new int[rayCount * 3];
 
         vertices[0] = origin;
 
-        int vertexIndex = 1;
-        int triangleIndex = 0;
-        for (int i = 0; i <= rayCount; i++) {
+        var vertexIndex = 1;
+        var triangleIndex = 0;
+        for (var i = 0; i <= rayCount; i++)
+        {
             Vector3 vertex;
-            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, UtilsClass.GetVectorFromAngle(angle), viewDistance, layerMask);
-            
-            if (raycastHit2D.collider == null) 
+            var raycastHit2D = Physics2D.Raycast(origin, UtilsClass.GetVectorFromAngle(angle), viewDistance, layerMask);
+            Debug.DrawLine(origin, raycastHit2D.point);
+            if (raycastHit2D.collider == null)
             {
-                // No hit
+                
                 vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
-            } 
-            else 
+            }
+            else
             {
                 // Hit object
                 if (raycastHit2D.collider.gameObject.tag == "Player")
                 {
+                    _movementScript.LastPlayerLocation = raycastHit2D.collider.gameObject.transform.position;
+                    _movementScript.StopMovement = false;
+                    //Debug.DrawLine(transform.position,_movementScript.LastPlayerLocation,Color.green);
                     Debug.Log("Player Spotted");
                 }
                 vertex = raycastHit2D.point;
             }
+
             vertices[vertexIndex] = vertex;
 
-            if (i > 0) {
+            if (i > 0)
+            {
                 triangles[triangleIndex + 0] = 0;
                 triangles[triangleIndex + 1] = vertexIndex - 1;
                 triangles[triangleIndex + 2] = vertexIndex;
@@ -74,30 +70,33 @@ public class FieldOfView : MonoBehaviour {
             }
 
             vertexIndex++;
-            angle -= angleIncrease;
+            angle += angleIncrease;
         }
 
 
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
-        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+        //mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
     }
 
-    public void SetOrigin(Vector3 origin) {
+    public void SetOrigin(Vector3 origin)
+    {
         this.origin = origin;
     }
 
-    public void SetAimDirection(Vector3 aimDirection) {
+    public void SetAimDirection(Vector3 aimDirection)
+    {
         startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) - fov / 2f;
     }
 
-    public void SetFoV(float fov) {
+    public void SetFoV(float fov)
+    {
         this.fov = fov;
     }
 
-    public void SetViewDistance(float viewDistance) {
+    public void SetViewDistance(float viewDistance)
+    {
         this.viewDistance = viewDistance;
     }
-
 }
--- BreakinGuns/Assets/Scripts/GunsBlazing/NPC_MovementScript.cs
+++ BreakinGuns/Assets/Scripts/GunsBlazing/NPC_MovementScript.cs
@@ -1,57 +1,81 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class NPC_MovementScript : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private FieldOfView _fieldOfView;
+    [SerializeField] private Vector3 WHAT;
+
+    [SerializeField] private GameObject Test;
+
     private bool _flip;
+    private bool _flipped;
+
+    private Rigidbody2D _rb;
+
+    public bool StopMovement = true;
+
+    public Vector3 LastPlayerLocation;
+
+
     public bool Flip
     {
-        get { return _flip; }
+        get => _flip;
         set
         {
-            if(_flip == true)
-            {
-                _flipped = !_flipped;
-            }
+            if (_flip) _flipped = !_flipped;
             transform.localScale = new Vector3(transform.localScale.x * -1f, transform.localScale.y, 1f);
             _flip = value;
         }
     }
 
-
-    private Rigidbody2D _rb;
-    private bool _flipped;
-    void Start()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
         _fieldOfView.SetOrigin(transform.position);
-        Vector2 direction = MoveNPC();
+        //Test.transform.position = WHAT;
+    }
+
+    private void Update()
+    {
+        
+        if (StopMovement) return;
+        //make it so that npc goes in direction of player using addforce
+        //if player is in field of view
+
+        var directionOfLastL = LastPlayerLocation - transform.position;
+        directionOfLastL = new Vector3(directionOfLastL.x, 0, 0);
+        directionOfLastL.Normalize();
+
+        _rb.AddForce(directionOfLastL * _speed);
+
+        if (directionOfLastL.x > 0)
+        {
+            if (LastPlayerLocation.x <= transform.position.x)
+                StopMovement = true;
+        }
+
+    }
+
+    private void RegularRightLeftMovement()
+    {
+        
+        var direction = MoveNPC();
 
         _rb.AddForce(direction * _speed);
     }
 
 
-
     private Vector2 MoveNPC()
     {
-
         if (Flip)
-        {
             return Vector2.right;
-        }
-        else
-        {
-            return Vector2.left;
-
-        }
+        return Vector2.left;
     }
 }
